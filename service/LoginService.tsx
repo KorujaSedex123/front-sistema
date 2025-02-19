ac8fb2730a4fc045e430155a94bfcc14283db38a
Koruja <74218234+KorujaSedex123@users.noreply.github.com>
2025-02-19 20:52:31 -0300

    first commit

---
+++ service/LoginService.tsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL_API
+})
+
+export class LoginService{
+
+    novoCadastro(usuario: Projeto.Usuario){
+        console.log("novoCadastro", usuario);
+        return axiosInstance.post("/auth/novoUsuario", usuario);
+    }
+
+    login(login: String, senha: String){
+        return axiosInstance.post("/auth/login", 
+            { username: login, password: senha});
+    }
+
+}
